2b26e5aeef914fcc259a3c087d858141ed6985d0
tatsu120 <canaryrig09210@gmail.com>
2026-09-19 18:27:01 +0900

    更新のため

---
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.samuraitravel.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.form.UserEditForm;
+import com.example.samuraitravel.security.UserDetailsImpl;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
+        User user = userDetailsImpl.getUser();
+
+        model.addAttribute("user", user);
+
+        return "user/index";
+    }
+
+    @GetMapping("/edit")
+    public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
+        User user = userDetailsImpl.getUser();
+        UserEditForm userEditForm = new UserEditForm(user.getName(), user.getFurigana(), user.getPostalCode(), user.getAddress(), user.getPhoneNumber(), user.getEmail());
+
+        model.addAttribute("userEditForm", userEditForm);
+
+        return "user/edit";
+    }    
+}
